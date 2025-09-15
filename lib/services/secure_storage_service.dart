e0a3e5cbd6a1b5945938c2b906102856d98e28fc
Ayush-K-Garg <017ayushgarg@gmail.com>
2025-09-15 19:12:42 +0530

    Update secure_storage_service.dart

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -20,8 +20,7 @@ class SecureStorageService {
     return null;
   }
 
-  // --- RENAMED THIS METHOD ---
-  // The method now matches the call in login_screen.dart
+
   Future<void> deleteCredentials() async {
     await _storage.deleteAll();
   }
